ef1d452965bc283d2a6c582c38764d11c7247411
MarcinB <marcio_br@interia.pl>
2017-12-13 11:01:42 +0100

    Add project files.

---
+++ 30_Days_of_Code/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _30_Days_of_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
